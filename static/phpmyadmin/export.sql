ece7ace18db2591a0d7a554603012a3c1cfbc4f3
Tudor Mateian <tudormateian@gmail.com>
2022-05-28 20:19:46 +0300

    added sql database export

---
+++ static/phpmyadmin/export.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 28, 2022 at 07:18 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `ewd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `imagedb`
+--
+
+CREATE TABLE `imagedb` (
+  `ID` int(11) NOT NULL,
+  `image` text COLLATE utf8_unicode_ci NOT NULL,
+  `name` text COLLATE utf8_unicode_ci NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `imagedb`
+--
+
+INSERT INTO `imagedb` (`ID`, `image`, `name`, `price`) VALUES
+(1, '5-dots.png', '5 Dots Pattern', 5),
+(2, 'circle-blues.webp', 'Circle Blues Pattern', 10),
+(3, 'cork-board.webp', 'Cork Board Pattern', 10),
+(4, 'wheat.webp', 'Wheat Pattern', 10),
+(5, 'pink-flowers.webp', 'Pink Flower Pattern', 5),
+(6, 'washi.webp', 'Washi Pattern', 5),
+(7, 'food.png', 'Food Pattern', 5),
+(8, 'watercolor.webp', 'Watercolor Pattern', 5),
+(9, 'bush.png', 'Bush Pattern', 5),
+(10, 'y-so-serious-white.png', 'Y so serious Pattern', 5),
+(11, 'dark-grey-terrazzo.webp', 'Dark Grey Terrazzo Pattern', 10),
+(12, 'darkness.webp', 'Darkness Pattern', 5);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `imagedb`
+--
+ALTER TABLE `imagedb`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `imagedb`
+--
+ALTER TABLE `imagedb`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
